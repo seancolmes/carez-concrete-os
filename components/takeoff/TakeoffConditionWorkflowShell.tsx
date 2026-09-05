e818fc5bc53c43cd1106e674f5475a6ed9a895a8
seancolmes <117563283+seancolmes@users.noreply.github.com>
2026-09-04 22:26:59 -0700

    feat(takeoff): enable integrated Direction A workstation

--- components/takeoff/TakeoffConditionWorkflowShell.tsx
+++ components/takeoff/TakeoffConditionWorkflowShell.tsx
@@ -1,35 +1,13 @@
 'use client';
 
-import { ConcreteConditionAuthoring } from './ConcreteConditionAuthoring';
-import { TakeoffDrawingWorkspace } from './TakeoffDrawingWorkspace';
+import {IntegratedTakeoffConditionWorkspace} from './IntegratedTakeoffConditionWorkspace';
 
-type Props = {
-  setId: string;
-  workspaceProps: any;
-  conditionData: any;
+type Props={
+  setId:string;
+  workspaceProps:any;
+  conditionData:any;
 };
 
-export function TakeoffConditionWorkflowShell({ setId, workspaceProps, conditionData }: Props) {
-  const conditionMeasurementIds = Array.from(new Set(
-    (conditionData?.roles || [])
-      .map((role: any) => role.measurement_id)
-      .filter(Boolean),
-  ));
-
-  return <>
-    <TakeoffDrawingWorkspace
-      {...workspaceProps}
-      conditionAuthoringActive
-      conditionMeasurementIds={conditionMeasurementIds}
-    />
-    <ConcreteConditionAuthoring
-      setId={setId}
-      locked={Boolean(workspaceProps.locked)}
-      data={conditionData}
-      measurements={workspaceProps.initialMeasurements || []}
-      sheets={workspaceProps.initialSheets || []}
-      assemblies={workspaceProps.assemblies || []}
-      assemblyVersions={workspaceProps.versions || []}
-    />
-  </>;
+export function TakeoffConditionWorkflowShell({setId,workspaceProps,conditionData}:Props){
+  return <IntegratedTakeoffConditionWorkspace setId={setId} workspaceProps={workspaceProps} conditionData={conditionData}/>;
 }
